fbee61b6c7cdc718d045e88308ae992eaf13ea53
alvaro javier <53539479+alvarojbucaro@users.noreply.github.com>
2021-11-07 08:09:08 -0500

    Update clientPostRequest.ts

--- src/apiClient/clientPostRequest.ts
+++ src/apiClient/clientPostRequest.ts
@@ -1,2 +1,2 @@
-import { postRequest } from 'xuach-commons'
-export const apiPostRequest = postRequest(process.env.XUACH_API_URL)
+import { postRequest } from "xuach-commons";
+export const apiPostRequest = postRequest(process.env.XUACH_API_URL);
