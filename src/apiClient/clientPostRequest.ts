1d5fa18e0c50dc40c4dbae3f8793cb1d0675d19d
alvaro javier <53539479+alvarojbucaro@users.noreply.github.com>
2021-11-07 07:53:41 -0500

    XCH-UPDATE-APICLIENT-TRANSLATOR

---
+++ src/apiClient/clientPostRequest.ts
@@ -0,0 +1,2 @@
+import { postRequest } from 'xuach-commons'
+export const apiPostRequest = postRequest(process.env.XUACH_API_URL)
--- src/i18N/home/TranslationsFooter.ts
+++ src/i18N/home/TranslationsFooter.ts
@@ -1,17 +1,10 @@
 export const LOCALES = {
   en: {
+    EXAMPLE:{
     message: "Hello!! - ENssssssss",
     tos: "Term of Service",
     term: "I accept {{1}} {{0}}.",
     loadbundle: "Load Bundle {{lang}}",
-  },
-  de: {
-    message: "Hallo!! - DE",
-    tos: "Nutzungsbedingungen",
-    term: "Ich akzeptiere {{0}}. {{1}}",
-    loadbundle: "Bundle Laden {{lang}}",
-  },
-  "es-ES": {
-    message: "yo soy una traduccion del lenguage detectado",
+    }
   },
 };
