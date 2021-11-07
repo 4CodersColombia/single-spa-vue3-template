7f4fb70dff25be004607c7972164e998beffa46f
alvaro javier <53539479+alvarojbucaro@users.noreply.github.com>
2021-11-07 07:56:04 -0500

    update translations template

--- src/i18N/ApiTranslator.ts
+++ src/i18N/ApiTranslator.ts
@@ -1,75 +1,69 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import i18next from 'i18next'
-const language = navigator.language.slice(0, 2)
+import i18next from "i18next";
+const language = navigator.language.slice(0, 2);
 
 export default class Translator {
-    private constructor(translations: any, key: string) {
-        if (i18next.isInitialized == undefined) {
-            this.postTranslate(translations)
-            return
-        }
-        const resourceLanguage = translations[language] ? language : 'en'
-        i18next.addResource(
-            resourceLanguage,
-            'translation',
-            key,
-            translations[resourceLanguage][key]
-        )
+  private constructor(translations: any, key: string) {
+    if (i18next.isInitialized == undefined) {
+      this.postTranslate(translations);
+      return;
     }
+    const resourceLanguage = translations[language] ? language : "en";
+    i18next.addResource(
+      resourceLanguage,
+      "translation",
+      key,
+      translations[resourceLanguage][key]
+    );
+  }
 
-    public getResourcesLanguages(
-        resources: Record<
-            string,
-            Record<string, string | Record<string, string>>
-        >
-    ) {
-        const keysLanguages = Object.keys(resources)
-        const languages = keysLanguages.map((item) => {
-            return { [item]: { translation: resources[item] } }
-        })
-        return languages.reduce((acum, current) => {
-            return {
-                ...acum,
-                ...current,
-            }
-        }, {})
-    }
+  public getResourcesLanguages(
+    resources: Record<string, Record<string, string | Record<string, string>>>
+  ) {
+    const keysLanguages = Object.keys(resources);
+    const languages = keysLanguages.map((item) => {
+      return { [item]: { translation: resources[item] } };
+    });
+    return languages.reduce((acum, current) => {
+      return {
+        ...acum,
+        ...current,
+      };
+    }, {});
+  }
 
-    public async postTranslate(
-        resources: Record<
-            string,
-            Record<string, string | Record<string, string>>
-        >,
-        resourceLanguage?: string
-    ) {
-        await i18next.init({
-            debug: true,
-            lng: resourceLanguage ? resourceLanguage : language,
-            resources: this.getResourcesLanguages(resources),
-        })
-    }
+  public async postTranslate(
+    resources: Record<string, Record<string, string | Record<string, string>>>,
+    resourceLanguage?: string
+  ) {
+    await i18next.init({
+      debug: true,
+      lng: resourceLanguage ? resourceLanguage : language,
+      resources: this.getResourcesLanguages(resources),
+    });
+  }
 
-    public static async changeLanguage(newLanguage: string) {
-        await i18next.changeLanguage(newLanguage, (err) => {
-            if (err) {
-                return console.log('something went wrong loading language', err)
-            }
-        })
-    }
+  public static async changeLanguage(newLanguage: string) {
+    await i18next.changeLanguage(newLanguage, (err) => {
+      if (err) {
+        return console.log("something went wrong loading language", err);
+      }
+    });
+  }
 
-    public static exists(nameSpace: string, key: string): boolean {
-        return i18next.exists(`${nameSpace}.${key}`)
-    }
+  public static exists(nameSpace: string, key: string): boolean {
+    return i18next.exists(`${nameSpace}.${key}`);
+  }
 
-    public static translate(nameSpace: string, key: string): string {
-        if (this.exists(nameSpace, key)) {
-            return i18next.t(`${nameSpace}.${key}`)
-        }
-        this.changeLanguage('en')
-        return i18next.t(`${nameSpace}.${key}`)
+  public static translate(nameSpace: string, key: string): string {
+    if (this.exists(nameSpace, key)) {
+      return i18next.t(`${nameSpace}.${key}`);
     }
+    this.changeLanguage("en");
+    return i18next.t(`${nameSpace}.${key}`);
+  }
 
-    public static getInstance(translations: any, key: string): Translator {
-        return new Translator(translations, key)
-    }
+  public static getInstance(translations: any, key: string): Translator {
+    return new Translator(translations, key);
+  }
 }
--- src/i18N/home/TranslationsFooter.ts
+++ src/i18N/home/TranslationsFooter.ts
@@ -1,10 +1,30 @@
-export const LOCALES = {
+import Translator from "../ApiTranslator";
+export const translations = {
   en: {
-    EXAMPLE:{
-    message: "Hello!! - ENssssssss",
-    tos: "Term of Service",
-    term: "I accept {{1}} {{0}}.",
-    loadbundle: "Load Bundle {{lang}}",
-    }
+    EXAMPLE: {
+      message: "Hello!! - ENssssssss",
+      tos: "Term of Service",
+      term: "I accept {{1}} {{0}}.",
+      loadbundle: "Load Bundle {{lang}}",
+    },
   },
 };
+type Translations = keyof typeof translations[keyof typeof translations];
+type TranslationsKeys =
+  keyof typeof translations[keyof typeof translations][Translations];
+
+const language = navigator.language.slice(0, 2);
+export default class ExampleTranslations {
+  private constructor() {
+    Translator.getInstance(translations, "EXAMPLE");
+  }
+  public translate(nameSpace: Translations, key: TranslationsKeys): string {
+    return Translator.translate(nameSpace, key);
+  }
+  public getTranslations(): typeof translations[keyof typeof translations] {
+    return translations[language];
+  }
+  public static getInstance(): ExampleTranslations {
+    return new ExampleTranslations();
+  }
+}
